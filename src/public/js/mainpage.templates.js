f520709fc6970cdaddc9e213babb7d219b3180a1
Marcelo Brazolim <marcelobrazolim@gmail.com>
2022-04-06 17:55:31 -0300

    js

--- src/public/js/mainpage.templates.js
+++ src/public/js/mainpage.templates.js
@@ -1,4 +1,25 @@
-function home_page() {
-    document.getElementById("mainpage_table").innerHTML='<div class="row-md-12"><h2>Home Page</h2></div><div class="mainpg_cont_home row"><div><img src="./img/spongebob-dance.gif" alt="a"></div></div>'
-    document.getElementById("sidebar_bottom").innerHTML='<ul><li on onclick="home_page()">Home Page</li><li>ipsum</li><ul><li>dolor</li><li>sit</li><li>amet</li></ul><li>consectetur</li><li>adipisicing</li><li>elit</li><ul><li>Vitae</li><li>eaque</li></ul><li>eiciendis</li><ul><li>ex</li></ul></ul><hr class="hr_contrast">'
+function nav(n) {
+    let elem = document.querySelectorAll(".nav_li")
+    for (let index = 0; index < elem.length; index++) {
+        document.getElementById("sidebar_bottom").getElementsByTagName("li")[index].classList.remove("active");
+    }
+    select(n, elem);
+    nav_page_selector(n)
+}
+function select(index, elem) {
+    elem[index].classList.add("active");
+}
+document.getElementById("mainpage_table").innerHTML='<div class="row-md-12"><h2>Home Page</h2></div><div class="mainpg_cont_home row"><div><img src="./img/spongebob-dance.gif" alt="a"></div></div>';
+function nav_page_selector(index) {
+    switch (index) {
+        case 0:
+            document.getElementById("mainpage_table").innerHTML='<div class="row-md-12"><h2>Home Page</h2></div><div class="mainpg_cont_home row"><div><img src="./img/spongebob-dance.gif" alt="a"></div></div>';
+            break;
+        case 1:
+            document.getElementById("mainpage_table").innerHTML='<div class="row-md-12"><h2>Home Page</h2></div><div class="mainpg_cont_home row"><div><img src="./img/dudu.jpg" alt="a"></div></div>';
+            break;
+    
+        default:
+            break;
+    }
 }
