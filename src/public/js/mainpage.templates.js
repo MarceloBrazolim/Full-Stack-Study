04832b17835c0b13abd8658b384cc9c44cf35494
Marcelo Brazolim <marcelobrazolim@gmail.com>
2022-04-07 17:36:11 -0300

    a

--- src/public/js/mainpage.templates.js
+++ src/public/js/mainpage.templates.js
@@ -1,12 +1,20 @@
+function head_span(n) {
+    let elem = document.querySelectorAll(".head")
+    for (let index = 0; index < elem.length; index++) {
+        document.getElementById("start").getElementsByTagName("span")[index].classList.remove("active");
+    }
+    select(elem, n);
+}
+
 function nav(n) {
-    let elem = document.querySelectorAll(".nav_li")
+    let elem = document.querySelectorAll(".nav")
     for (let index = 0; index < elem.length; index++) {
         document.getElementById("sidebar_bottom").getElementsByTagName("li")[index].classList.remove("active");
     }
-    select(n, elem);
+    select(elem, n);
     nav_page_selector(n)
 }
-function select(index, elem) {
+function select(elem, index) {
     elem[index].classList.add("active");
 }
 document.getElementById("mainpage_table").innerHTML='<div class="row-md-12"><h2>Home Page</h2></div><div class="mainpg_cont_home row"><div><img src="./img/spongebob-dance.gif" alt="a"></div></div>';
